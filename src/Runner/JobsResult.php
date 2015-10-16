2614cd605ec7a8cd64a7940f2c97a9130114dc72
marcogiovinazzi <marco.giovinazzi@comodojo.org>
2015-10-17 00:15:07 +0200

    more tests

--- src/Runner/JobsResult.php
+++ src/Runner/JobsResult.php
@@ -78,8 +78,7 @@ class JobsResult {
                 'start'  =>  $process[3],
                 'end'    =>  $process[4],
                 'result' =>  $process[5],
-                'id'     =>  $process[6]//,
-                //'uid'    =>  $process[7]
+                'id'     =>  $process[6]
             ));
 
         }
